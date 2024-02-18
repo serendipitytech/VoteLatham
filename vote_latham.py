9cc42cf27c8e515a6070c98491149510bc277325
serendipitytech <troy@serendipitytech.net>
2024-02-18 16:50:19 -0500

    Update vote_latham.py

--- vote_latham.py
+++ vote_latham.py
@@ -64,8 +64,8 @@ def main():
         df['Age_Range'] = df['Age'].apply(categorize_age)
         
         # Sidebar filters
-        logo_url = "https://votelatham.com/assets/images/logo_1.png"
-        st.sidebar.image(logo_url, width=200)
+        logo_url = "https://votelatham.com/assets/images/logo/logo_lightbg.png"
+        st.sidebar.image(logo_url, width=300)
         st.sidebar.subheader("Filters")
         selected_status = st.sidebar.selectbox("Select Status", df['Voter_Status'].unique(), index=df['Voter_Status'].unique().tolist().index('ACT'))
         selected_race = st.sidebar.multiselect("Select Race", df['Race'].unique(), default=df['Race'].unique())
